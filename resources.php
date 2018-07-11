a7477af32d16d41996d10c05ad798f5fd18d4a7e
Alex Lynd <alexlynd@protonmail.com>
2018-07-11 16:37:37 -0700

    Rename

---
+++ resources.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Resources</title>
+		<link rel="stylesheet" type= "text/css" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>	
+	<body>
+		<?php include "header.php"; ?>	
+		<div class="main">
+			<p>Nothing here yet...Just testing </p>
+			</div>
+			<div class="imghold">
+				<div class="figure">
+					<img src="images/raspberry_pi_icon_low.jpg" height="90%" width="90%">
+				  	<p>PI</p>
+				</div>
+
+				<div class="figure">
+					<img src="images/raspberry_pi_icon_low.jpg" height="90%" width="90%">
+				  	<p>PI</p>
+			</div>
+
+			</div>
+		</body>
+</html>
