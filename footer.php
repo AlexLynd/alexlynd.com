efca1d0278874d25be070196268cf826d52d1785
Alex Lynd <alexlynd@protonmail.com>
2018-07-22 22:47:55 -0700

    Nothing really

--- footer.php
+++ footer.php
@@ -1,12 +1,7 @@
-		<header>          
-			 <div class= "titleimg">
-				<img src= "images/raspberry_pi_models.jpg" alt= "rpi"/>
-				<div class="centered"><h1>Home</h1></div>
-            </div>		
+		<footer>          		
             <ul class= "navbar">
                 <li  class= "navbar" style="margin-left: 50px"><a class="active" href="index.php">Home</a></li>
                 <li class= "navbar"><a href="projects.php">Projects</a></li>
-				<li class= "navbar"><a href="resources.php">Tutorials</a></li>
                 <li class= "navbar"><a href="resources.php">Resources</a></li>
                 <li class= "navbar"><a href="guides.php">Guides</a></li>
                 <li class= "navbar" style="float:right; margin-right: 50px"><a href= "https://www.youtube.com/channel/UC92rvEjR-5ggjVUotRK8UQA" target="_blank" class="fa fa-youtube"></a></li> 
@@ -14,4 +9,4 @@
 				<li class= "navbar" style="float:right"><a href= "https://twitter.com/AlexLynd/" target="_blank" class="fa fa-twitter"></a></li> 
 				<li class= "navbar" style="float:right"><a href= "https://github.com/AlexLynd/" target="_blank" class="fa fa-github"></a></li> 
             </ul>
-		</header>
+		</footer>
