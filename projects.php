092a41ab5a65996556bf150e4b18cf39a18b6e4b
Alexander Lynd <alexander.lynd@smhsstudents.org>
2018-12-14 12:49:34 -0800

    Change html to php

---
+++ projects.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>Projects</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+	</head>
+	<body>
+		<?php include "navbar.html";?>
+		<main class= "special">
+			<div class="row"> 
+				<div class="column">
+					<img src="projects/pic/foo.JPG">
+					<h3 style="margin:0">ReconPiReconPiReconPiReconPiReconPiReconPiReconPivReconPiReconPiReconPi</h3>
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+				</div>
+				<div class="column">
+					<img src="projects/pic/foo.JPG">
+					<h3 style="margin:0">6 Function Calculator Hack</h3>
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+				</div>
+				<div class="column">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+					<img src="projects/pic/foo.JPG">
+				</div>
+			</div>
+		</main>		
+	</body>
+</html>
