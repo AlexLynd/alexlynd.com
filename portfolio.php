08cf24a4ec06c6da554eba9c4d9043d0e7c1786f
Alexander Lynd <alexander.lynd@smhsstudents.org>
2018-12-14 12:49:23 -0800

    Add portfolio page

---
+++ portfolio.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>Portfolio</title>
+		<link rel="stylesheet" type= "text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+	</head>
+	<body>
+		<?php include "navbar.html";?>	
+		<main>
+		<h1 class="type">Alex Lynd</h1>
+		<p>Welcome to my website!  This is my personal page where you can find my project portfolio, my story, and resources and tutorials on various things including programming, hacking and more.  This page is curently under development and you can view the source <a href= "https://github.com/alexlynd/alexlynd.com_source">here</a>.</p>
+		</main>
+		<sidebar>
+			<!--img src="IMG_3058.JPG" style="width: 400px" -->
+		</sidebar>
+	</body>
+</html>
