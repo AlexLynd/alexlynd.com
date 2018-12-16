be5286c35b93ca50e65f81d5ee5766eb5b490cee
Alexander Lynd <alexander.lynd@smhsstudents.org>
2018-12-15 21:23:00 -0800

    Typed.js stuff

--- index.php
+++ index.php
@@ -7,8 +7,26 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+		<script src="typed.js" type="text/javascript"></script>
 	</head>
 	<body>
+		<script>
+			document.addEventListener('DOMContentLoaded',function() {
+				var typed = new Typed('#typed', {
+					strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+					typeSpeed: 40,
+					backSpeed: 0,
+					loop: true
+				});
+				var typed2 = new Typed('#typed2', {
+					strings: ['My strings are: <i>strings</i>; with', 'My strings are: &lt;strong&gt;HTML&lt;/strong&gt;', 'My strings are: Chars &amp;times; &amp;copy;'],
+					typeSpeed: 0,
+					backSpeed: 0,
+					smartBackspace: true,
+					loop: true
+				});
+			});
+	</script>
 		<?php include "navbar.html";?>	
 		<main>
 		<h1 class="type">Alex Lynd</h1>
