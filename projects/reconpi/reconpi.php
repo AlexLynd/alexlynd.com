3744b0fa87537384861920f596fae927dc502f1d
Alexander Lynd <alexander.lynd@smhsstudents.org>
2018-12-20 18:41:03 -0800

    Move file

--- projects/reconpi/reconpi.php
+++ projects/reconpi/reconpi.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="utf-8">
 		<title>ReconPi</title>
-		<link rel="stylesheet" type= "text/css" href="style.css">
+		<link rel="stylesheet" type= "text/css" href="../../style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
-		<script src="typed.js" type="text/javascript"></script>
+		<script src="../../typed.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<script>
@@ -28,7 +28,7 @@
 				});
 			});
 		</script>
-		<?php include "navbar.html";?>	
+		<?php include "../../navbar.html";?>	
 		<main>
 			<titlewrap> <span id="typed" style="white-space:pre;"></span> </titlewrap>
 			<p></p> <span id="typed2"></span>
