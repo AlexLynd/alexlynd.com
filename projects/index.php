f717b472c74b0cd5af522185413def8e43056eff
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-08 16:03:02 -0700

    Some stuff

--- projects/index.php
+++ projects/index.php
@@ -1,14 +1,16 @@
+<!-- PROJECTS -->
 <!doctype html>
 <html lang= "en-US">
 	<head>
 		<meta charset="utf-8">
 		<title>Projects</title>
-		<link rel="stylesheet" type= "text/css" href="../css/main.css">
+		<link rel="stylesheet" type= "text/css" href="../css/projects.css">
 		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
 		<script src="../typed.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
 	<body>
 		<script>
@@ -30,9 +32,11 @@
 			});
 		</script>
 		<?php include "../menu_low.html";?>	
+		<header>
+			<div class= "title"><span id="typed" style="white-space:pre;"></span></div>
+		</header>
 		<main>
-			<titlewrap> <span id="typed" style="white-space:pre;"></span> </titlewrap>
-			<p></p> <span id="typed2"></span>
+			<p><a href="TI-Codes_2019.php">TI-Codes 2019</a></p>
 		</main>
 	</body>
 </html>
