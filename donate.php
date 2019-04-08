1d5f8f1643708dc87ba096068f013e73547fc423
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-08 16:02:16 -0700

    Give me free stuff

---
+++ donate.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>Support me!</title>
+		<link rel="stylesheet" type= "text/css" href="css/projects.css">
+		<link rel="stylesheet" type= "text/css" href="css/menu.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+		<script src="../typed.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
+	</head>
+	<body>
+		<script>
+			document.addEventListener('DOMContentLoaded',function() {
+				var typed = new Typed('#typed', {
+					strings: ['<b>Donate&nbsp;</b>'],
+					typeSpeed: 10,
+					backSpeed: 0,
+					loop: false
+				});
+				var typed2 = new Typed('#typed2', {
+					strings: ['^500Give me free stuff'],
+					typeSpeed: 0,
+					backSpeed: 0,
+					smartBackspace: true,
+					showCursor: false,
+					loop: false
+				});
+			});
+		</script>
+		<?php include "menu.html";?>	
+		<header>
+			<div class= "title"><span id="typed" style="white-space:pre;"></span></div>
+		</header>
+		<main>
+			<p><a href="https://patreon.com/alexlynd">Patreon</a></p>
+			<p><a href="https://buymeacoffee.com/alexlynd">Buy Me a Coffee </a></p>
+		</main>
+	</body>
+</html>
