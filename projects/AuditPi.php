2e937a6598d4f6650e49b2150eb42b2ba8f814f5
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-17 16:14:04 -0700

    testing for first blog thing

---
+++ projects/AuditPi.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>AuditPi</title>
+		<link rel="stylesheet" type= "text/css" href="../css/post.css">
+		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
+	</head>
+	<body>
+		<div id="particles-js"></div>
+		<script src="../js/particles.js"></script>
+		<script src="../home.js"></script>
+		<div class="text">
+			<?php include "../menu_low.html";?>
+		<header>
+			<div class= "title">AuditPi 1.0</div>
+			<div class= "description">A Modular Handheld Raspberry Pi PDA <i class="fas fa-link"></i> <i class="fab fa-github-alt"></i>
+				<i class="fab fa-youtube"></i></div>
+			<!--center> <iframe width="900" height="385" src=""></iframe></center-->
+		</header>
+		<main>
+			<p>Official documentation for the AuditPi project.  Check blog posts for updates.</p>
+			 <h3>Description</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nisl nunc mi ipsum faucibus vitae. Amet risus nullam eget felis. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Dui nunc mattis enim ut tellus. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Elementum pulvinar etiam non quam lacus. Enim nec dui nunc mattis enim ut. Congue eu consequat ac felis donec.
+
+Feugiat vivamus at augue eget arcu. Mi eget mauris pharetra et ultrices. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Dui sapien eget mi proin sed libero enim. Aliquet bibendum enim facilisis gravida neque convallis. Lobortis scelerisque fermentum dui faucibus in ornare quam. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Et netus et malesuada fames ac turpis egestas maecenas. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Sed odio morbi quis commodo odio aenean sed. Lectus nulla at volutpat diam ut venenatis tellus in. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Purus ut faucibus pulvinar elementum integer.
+
+In vitae turpis massa sed elementum. Neque convallis a cras semper. Arcu non odio euismod lacinia at quis risus. Nunc sed id semper risus. Amet cursus sit amet dictum. Diam sollicitudin tempor id eu nisl nunc. Aliquam vestibulum morbi blandit cursus risus at ultrices. Ullamcorper malesuada proin libero nunc consequat interdum varius. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Tortor condimentum lacinia quis vel eros donec ac. Vel pharetra vel turpis nunc eget lorem dolor sed.
+
+Blandit volutpat maecenas volutpat blandit. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Enim diam vulputate ut pharetra sit amet aliquam id. Duis at consectetur lorem donec massa sapien. Eget mi proin sed libero enim sed. Tristique senectus et netus et malesuada fames. Arcu non odio euismod lacinia at. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. Quis commodo odio aenean sed. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.
+
+Eu facilisis sed odio morbi quis commodo odio aenean sed. Velit ut tortor pretium viverra suspendisse. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Dictum fusce ut placerat orci nulla pellentesque. Viverra vitae congue eu consequat ac felis donec. Faucibus in ornare quam viverra. Eget aliquet nibh praesent tristique magna sit amet purus. Egestas erat imperdiet sed euismod. Quam adipiscing vitae proin sagittis nisl rhoncus. Duis convallis convallis tellus id interdum. In tellus integer feugiat scelerisque varius. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Elementum sagittis vitae et leo duis ut.</p>
+			<h3>Material</h3>
+		</main>
+		<sidebar>
+			<h3>Gallery</h3> <br>
+			<h3>Files</h3>
+			<p><a href="">Download all</a> (ZIP)</p>
+			<p><a href="">sch01.png</a> (SCH)</p>
+			<br>
+			<h3>Links</h3>
+			<p><a href= "https://education.ti.com/en/promotion/rules/ti-codes-contest-rules">Official Rules</a></p>
+		</sidebar>
+		</div>
+	</body>
+</html>
