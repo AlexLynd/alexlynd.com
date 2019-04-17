9bcb907a76900b19ddb9c38abb8c074885a9c387
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-17 16:11:50 -0700

    move portfolio as index

---
+++ portfolio/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+	    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>Portfolio</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type= "text/css" href="../css/portfolio.css">
+		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+		<script src="../js/typed.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<script>
+		document.addEventListener('DOMContentLoaded',function() {
+				var typed = new Typed('#typed', {
+					strings: ['Competitions and other achievements.'],
+					typeSpeed: 0,
+					backSpeed: 0,
+					smartBackspace: true,
+					showCursor: false,
+					loop: false
+				});
+			});
+		</script>
+		<div id="particles-js"></div>
+		<script src="../js/particles.js"></script>
+		<script src="../home.js"></script>
+		<div class="text">
+			<?php include "../menu_low.html";?>
+			<header>
+				<div class= "title">Portfolio</div>
+				<center><span id="typed" style="white-space:pre;"></span></center>
+			</header>
+			<main>
+				<div class="container">   
+					<div class="element"><div class="post"><h1>TI-Codes 2019</h1><p>Progress for my entry in TI-Codes 2019</p></div></div>
+					<div class="element"><div class="post"><h1>ACSL</h1></div></div>
+					<div class="element"><div class="post"><h1>This Website</h1></div></div>
+					<div class="element"><div class="post"><h1>test</h1></div></div>
+					<div class="element"><div class="post"><p>test</p></div></div>
+					<div class="element"><div class="post"><p>test</p></div></div>
+				</div>
+			</main>
+		</div>
+	</body>
+</html>
