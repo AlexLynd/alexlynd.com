47a79ec91fbca022512eb7151e5f867d885b77b0
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-17 16:12:22 -0700

    reference

--- donate.php
+++ donate.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Support me!</title>
-		<link rel="stylesheet" type= "text/css" href="css/projects.css">
+		<link rel="stylesheet" type= "text/css" href="css/portfolio.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
-		<script src="../typed.js" type="text/javascript"></script>
+		<script src="js/typed.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
 	<body>
@@ -35,6 +35,7 @@
 			<div class= "title"><span id="typed" style="white-space:pre;"></span></div>
 		</header>
 		<main>
+			<p>These links haven't been fully configured yet, but thanks for checking it out!</p>
 			<p><a href="https://patreon.com/alexlynd">Patreon</a></p>
 			<p><a href="https://buymeacoffee.com/alexlynd">Buy Me a Coffee </a></p>
 		</main>
