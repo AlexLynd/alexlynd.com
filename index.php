0a6d5ff228155bd8aad41d954dbd9ab5afa696d1
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-17 16:14:33 -0700

    Stuff

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<!-- MAIN -->
 <!doctype html>
 <html lang= "en-US">
 	<head>
 		<meta charset="utf-8">
 		<title>Alex Lynd's Portfolio | Hacks, Fire and Electronics</title>
-		<link rel="stylesheet" type= "text/css" href="css/main.css">
+		<link rel="stylesheet" type= "text/css" href="css/home.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 
-		<head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="js/typed.js" type="text/javascript"></script>
 	</head>
@@ -18,9 +16,8 @@
 			document.addEventListener('DOMContentLoaded',function() {
 				var typed = new Typed('#typed', {
 					strings: ['<b>Alex Lynd&nbsp;</b>'],
-					typeSpeed: 10,
-					backSpeed: 0,
-					loop: false
+					    typeSpeed: 50,
+						loop: false
 				});
 				var typed2 = new Typed('#typed2', {
 					strings: ['^500Welcome to my website!  This is my personal page where you can find my project portfolio, my story, and resources and tutorials on various things including programming, hacking and more.  This page is curently under development and you can view the source <a href= "https://github.com/alexlynd/alexlynd.com_source">here</a>.'],
@@ -40,19 +37,16 @@
 				});
 			});
 		</script>
-		<?php include "menu.html";?>
 		<div id="particles-js"></div>
 		<script src="js/particles.js"></script>
 		<script src="home.js"></script>
 		<div class= "text">
-		<main>
-			
+			<?php include "menu.html";?>
+			<main>
 				<titlewrap> <span id="typed" style="white-space:pre;"></span> </titlewrap>
 				<p></p> <span id="typed2"></span> <p></p>
-				<div class="iconwrap"><span id="typed3"></span></iconwrap>
-				</div>
-			
-		</main>
+				<div class="iconwrap"><span id="typed3"></span></div>
+			</main>
 		</div>
 	</body>
 </html>
--- projects/index.php
+++ projects/index.php
@@ -1,28 +1,21 @@
-<!-- PROJECTS -->
 <!doctype html>
 <html lang= "en-US">
 	<head>
-		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<title>Projects</title>
-		<link rel="stylesheet" type= "text/css" href="../css/projects.css">
+		<meta charset="utf-8">
+		<link rel="stylesheet" type= "text/css" href="../css/projects.css"> <!-- excise -->
 		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
 		<script src="../js/typed.js" type="text/javascript"></script>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
 	<body>
 		<script>
-			document.addEventListener('DOMContentLoaded',function() {
+		document.addEventListener('DOMContentLoaded',function() {
 				var typed = new Typed('#typed', {
-					strings: ['<b>Projects&nbsp;</b>'],
-					typeSpeed: 10,
-					backSpeed: 0,
-					loop: false
-				});
-				var typed2 = new Typed('#typed2', {
-					strings: ['^500Projects and things I&#39;ve done.'],
+					strings: ['Things I have created.'],
 					typeSpeed: 0,
 					backSpeed: 0,
 					smartBackspace: true,
@@ -31,12 +24,25 @@
 				});
 			});
 		</script>
-		<?php include "../menu_low.html";?>	
-		<header>
-			<div class= "title"><span id="typed" style="white-space:pre;"></span></div>
-		</header>
-		<main>
-			<p><a href="TI-Codes_2019.php">TI-Codes 2019</a></p>
-		</main>
+		<div id="particles-js"></div>
+		<script src="../js/particles.js"></script>
+		<script src="../home.js"></script>
+		<div class="text">
+			<?php include "../menu_low.html";?>
+			<header>
+				<div class= "title">Projects</div>
+				<center><span id="typed" style="white-space:pre;"></span></center>
+			</header>
+			<main>
+				<div class="container">   
+					<div class="element"><div class="post"><h1>AuditPi</h1><a href="AuditPi.php"><p>Handheld Raspberry Pi PDA</p></a></div></div>
+					<div class="element"><div class="post"><h1>ACSL</h1></div></div>
+					<div class="element"><div class="post"><h1>This Website</h1></div></div>
+					<div class="element"><div class="post"><h1>test</h1></div></div>
+					<div class="element"><div class="post"><p>test</p></div></div>
+					<div class="element"><div class="post"><p>test</p></div></div>
+				</div>
+			</main>
+		</div>
 	</body>
 </html>
