9a634ae9f99e4319c93d124d016b87f9dd8eb535
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-04-24 13:43:04 -0700

    some stuff testing

--- index.php
+++ index.php
@@ -14,20 +14,20 @@
 	<body>
 		<script>
 			document.addEventListener('DOMContentLoaded',function() {
-				var typed = new Typed('#typed', {
+				var typed = new Typed('.typed', {
 					strings: ['<b>Alex Lynd&nbsp;</b>'],
 					    typeSpeed: 50,
 						loop: false
 				});
 				var typed2 = new Typed('#typed2', {
-					strings: ['^500Welcome to my website!  This is my personal page where you can find my project portfolio, my story, and resources and tutorials on various things including programming, hacking and more.  This page is curently under development and you can view the source <a href= "https://github.com/alexlynd/alexlynd.com_source">here</a>.'],
+					strings: ['^500Welcome to my website! This is my personal page where you can find my project documentation, portfolio, story, and other resources/tutorials on a variety of things including programming, hacking and more. This site is programmed and maintained by me, and you can view the source <a href="https://github.com/alexlynd/website">here</a>.  Click the icons below to head to my social media, or one of the links in the menu to check out my stuff!'],
 					typeSpeed: 0,
 					backSpeed: 0,
 					smartBackspace: true,
 					showCursor: false,
 					loop: false
 				});
-				var typed3 = new Typed('#typed3', {
+				var typed3 = new Typed('.iconwrap', {
 					strings: ['^3500<a href="https://github.com/alexlynd" target="_blank"><i class="fab fa-github-alt"></i></a><i class="fab fa-youtube"></i><a href= "https://instagram.com/alex_lynd" target="_blank"><i class="fab fa-instagram"></i></a><a href= "https://twitter.com/alexlynd" target="_blank"><i class="fab fa-twitter"></i></a><i class="fab fa-patreon"></i>'],
 					typeSpeed: 0,
 					backSpeed: 0,
@@ -43,9 +43,9 @@
 		<div class= "text">
 			<?php include "menu.html";?>
 			<main>
-				<titlewrap> <span id="typed" style="white-space:pre;"></span> </titlewrap>
+				<titlewrap><span class="typed" style="white-space:pre;"></span></titlewrap>
 				<p></p> <span id="typed2"></span> <p></p>
-				<div class="iconwrap"><span id="typed3"></span></div>
+				<span class="iconwrap"></span>
 			</main>
 		</div>
 	</body>
---
+++ projects/AuditPi/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>AuditPi</title>
+		<link rel="stylesheet" type= "text/css" href="../../css/post.css">
+		<link rel="stylesheet" type= "text/css" href="../../css/menu.css">
+		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
+	</head>
+	<body>
+		<div id="particles-js"></div>
+		<script src="../../js/particles.js"></script>
+		<script src="../../home.js"></script>
+		<div class="text">
+			<?php include "../../menu_low_third.html";?>
+		<header>
+			<div class= "title">AuditPi 1.0</div>
+			<div class= "description">A Modular Handheld Raspberry Pi PDA <i class="fas fa-link"></i> <i class="fab fa-github-alt"></i> <i class="fab fa-youtube"></i>
+			</div>
+			
+			<center><iframe width="704" height="396" src="https://www.youtube.com/embed/--w1xGTmC7Y"></iframe> </center>
+		</header>
+		<main>
+			<p>Official documentation for the AuditPi project.  Check blog posts for updates.</p>
+			 <h3>Description</h3>
+			<p>Content coming soon! </p>
+			<h3>Material</h3>
+		</main>
+		<sidebar>
+			<h3>Gallery</h3> <br>
+			<h3>Files</h3>
+			<p><a href="">Download all</a> (ZIP)</p>
+			<p><a href="">sch01.png</a> (SCH)</p>
+			<br>
+			<h3>Links</h3>
+			<p></p>
+		</sidebar>
+		</div>
+	</body>
+</html>
