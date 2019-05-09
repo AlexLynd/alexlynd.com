4d05876990f46f4b5c31d22ddfd9a80acd62a47b
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-05-09 02:04:59 -0700

    remove stupid typing

--- donate.php
+++ donate.php
@@ -6,28 +6,17 @@
 		<link rel="stylesheet" type= "text/css" href="css/portfolio.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<script src="js/typed.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
 	<body>
-		<script>
-			document.addEventListener('DOMContentLoaded',function() {
-				var typed = new Typed('#typed', {
-					strings: ['<b>Donate&nbsp;</b>'],
-					typeSpeed: 10,
-					backSpeed: 0,
-					loop: false
-				});
-			});
-		</script>
 		<?php include "menu.html";?>	
 		<header>
-			<div class= "title"><span id="typed" style="white-space:pre;"></span></div>
+			<div class= "title">Donate</div>
 		</header>
 		<main>
 			<p>These links haven't been fully configured yet, but thanks for checking it out!</p>
 			<p><a href="https://patreon.com/alexlynd">Patreon</a></p>
-			<p><a href="https://buymeacoffee.com/alexlynd">Buy Me a Coffee </a></p>
+			<p><a href="https://buymeacoffee.com/alexlynd">Buy Me a Coffee</a></p>
 		</main>
 	</body>
 </html>
