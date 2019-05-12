f52a3d7c34db67575b179dc2d5fbb930f019e8c9
Alexander Lynd <alexander.lynd@smhsstudents.org>
2019-05-12 00:17:20 -0700

    Some stuff

--- projects/AuditPi/1.0.php
+++ projects/AuditPi/1.0.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" type= "text/css" href="../../css/menu.css">
 		<link href="https://fonts.googleapis.com/css?family=Space Mono" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Work Sans" rel="stylesheet">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 	</head>
 	<body>
 			<?php include "../../menu.html";?>
