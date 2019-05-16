a6cfcea237d06fa9c1296aff6ad1ff2a8075ec21
AlexLynd <alexlynd@protonmail.com>
2019-05-15 18:32:08 -0700

    More readable code

--- projects/index.php
+++ projects/index.php
@@ -6,66 +6,61 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type= "text/css" href="../css/portfolio.css">
 		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
-		<script src="../js/typed.js" type="text/javascript"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
-		<div class= "text">
-			<?php include "../menu.html";?>
-			
-			<header>
-				<div class= "title">Projects</div>
-				<center><p>Project docs and things I create. Check portfolio for more 'official' type stuff.</p></center>
-			</header>
-			<main>
-				<div class="container"> 
-					<div class="element">
-						<div class="post">
-							<img src="esp.JPG">
-						</div>
-						<div class="cappy">
-							<h1>ESP-DriveBy</h1>
-							<p>Wardriving with the ESP8266</p>
-						</div>
+		<?php include "../menu.html";?>
+		<header>
+			<div class= "title">Projects</div>
+			<div class="description" id="sub-page">Project documentation for stuff I make. Check portfolio for contests and resume content.</div>
+		</header>
+		<main>
+			<div class="post-container"> 
+				<div class="post">
+					<div class="post-icon">
+						<img src="esp.JPG">
 					</div>
-					<div class="element">
-						<div class="post">
-							<a href= "AuditPi"><img src="AuditPi/IMG_3891.jpg"></a>
-						</div>
-						<div class="cappy">
-							<h1>AuditPi</h1>
-							<p>A modular hackable Raspberry Pi PDA</p>
-						</div>
+					<div class="post-caption">
+						<h1>ESP-DriveBy</h1>
+						<p>Wardriving with the ESP8266</p>
 					</div>
-					<div class="element">
-						<div class="post">
-							<a href= "IR-Tester"><img src="IR-Tester/img/IR-Tester.jpg"></a>
-						</div>
-						<div class="cappy">
-							<h1>IR-Tester</h1>
-							<p>Grab a quick IR value / protocol!</p>
-						</div>
-					</div>
-					<div class="element">
-						<div class="post">
-							<a href="CalcProbe"><img src="testttt.JPG"></a>
-						</div><div class="cappy">
-						<h1>CalcProbe</h1>
-						<p>Exploiting 6 Function Calculators</p>
-						</div>
-					</div>
-					<div class="element">
-						<div class="post">
-							<img src="fug.jpg">
-						</div>
-						<div class="cappy">
-							<h1>TrashSort</h1>
-							<p>AI based trash classification/sorting</p>
-						</div>
-					</div>
-					
 				</div>
-			</main>
-		</div>
+				<div class="post">
+					<div class="post-icon">
+						<a href= "AuditPi"><img src="AuditPi/IMG_3891.jpg"></a>
+					</div>
+					<div class="post-caption">
+						<h1>AuditPi</h1>
+						<p>A modular hackable Raspberry Pi PDA</p>
+					</div>
+				</div>
+				<div class="post">
+					<div class="post-icon">
+						<a href= "IR-Tester"><img src="IR-Tester/img/IR-Tester.jpg"></a>
+					</div>
+					<div class="post-caption">
+						<h1>IR-Tester</h1>
+						<p>Grab a quick IR value / protocol!</p>
+					</div>
+				</div>
+				<div class="post">
+					<div class="post-icon">
+						<a href="CalcProbe"><img src="testttt.JPG"></a>
+					</div><div class="post-caption">
+					<h1>CalcProbe</h1>
+					<p>Exploiting 6 Function Calculators</p>
+					</div>
+				</div>
+				<div class="post">
+					<div class="post-icon">
+						<img src="fug.jpg">
+					</div>
+					<div class="post-caption">
+						<h1>TrashSort</h1>
+						<p>AI based trash classification/sorting</p>
+					</div>
+				</div>
+			</div>
+		</main>
 	</body>
 </html>	
