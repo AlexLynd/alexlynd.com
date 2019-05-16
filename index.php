5d315bebdbada15336d8921663769d6a9bfdda91
AlexLynd <alexlynd@protonmail.com>
2019-05-15 18:33:42 -0700

    Revert to old

--- index.php
+++ index.php
@@ -23,10 +23,11 @@
 		<script src="js/particles.js"></script>
 		<script src="home.js"></script>
 		<div class= "text">
-			<?php include "menu.html";?>
+			<header>
+				<?php include "menu.html";?>
+			</header>
+			
 			<main>
-				<div class="section-wrap">
-					<div class= "section1">
 						<titlewrap><span class="typed" style="white-space:pre;"></span></titlewrap>
 						<p class= "float-fade-in">Welcome to my website! This is my personal page where you can find my project documentation, portfolio, story, and other resources/tutorials on a variety of things including programming, hacking and more. This site is programmed and maintained by me, and you can view the source <a href="https://github.com/alexlynd/website">here</a>.  Click the icons below to head to my social media, or one of the links in the menu to check out my stuff!</p>
 						<div class="iconwrap">
@@ -36,32 +37,6 @@
 							<a href= "https://twitter.com/alexlynd" target="_blank"><i class="fab fa-twitter"></i></a>
 							<a href="https://patreon.com/alexlynd" target="_blank"><i class="fab fa-patreon"></i></a>
 						</div>
-					</div>
-				</div>
-				<div class="section-wrap">
-					<div class="section1">
-						<br><br><br><br><br>
-					</div>
-				</div>
-				<div class="section-wrap" id="block3">
-					<div class="section1" style="float: right; width: 40%">
-						<a name="about"><h1 style="color: #111111">About Me</h1></a>
-						<p>Hey! I'm Alex Lynd, a 16 year old hobbyist hacker/maker from California. My hobbies include hardware hacking, blowing stuff up and messing with Wi-Fi.  I want to get into infosec and learn more about SIGINT, and spend my free time learning about stuff like this.  I make a lot of cool projects and haxx, which you can find on my page here.</p>
-					</div>
-					<div class="section1" style="float: right; width: 40%">
-						<img src="tmp/parrot.jpg" width=100%;>
-					</div>
-				</div>
-				<div class="section-wrap" id="block2">
-					<div class="section1" style="float: right; width: 30%">
-						<img src="tmp/parrot.jpg" width=100%;>
-					</div>
-					<div class="section1" style="float: right">
-						<h1>About This Page</h1>
-						<p>I wrote this entire website from scratch using html, css, and js.  Even though the extensions are PHP files, I'm only using the PHP include function to include menus.</p>
-					</div>
-					
-				</div>
 			</main>
 		</div>
 	</body>
