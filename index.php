65283e4b040e779030b1ede0223aee8f61bc052a
AlexLynd <alexlynd@protonmail.com>
2019-05-16 11:56:28 -0700

    cool header

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
+<!-- HOME -->
+
+<!--
+    ___    __             __                    __                           
+   /   |  / /__  _  __   / /   __  ______  ____/ /       _________  ____ ___ 
+  / /| | / / _ \| |/_/  / /   / / / / __ \/ __  /       / ___/ __ \/ __ `__ \
+ / ___ |/ /  __/>  <   / /___/ /_/ / / / / /_/ /  _    / /__/ /_/ / / / / / /
+/_/  |_/_/\___/_/|_|  /_____/\__, /_/ /_/\__,_/  (_)   \___/\____/_/ /_/ /_/ 
+                            /____/                                           
+feel free to snoop! | GitHub @alexlynd | Instagram @alex_lynd | Twitter @alexlynd
+-->
+
 <!doctype html>
 <html lang= "en-US">
 	<head>
-		<meta charset="utf-8">
 		<title>Alex Lynd's Portfolio | Hacks, Fire and Electronics</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Haxx and spooky stuff">
+  		<meta name="keywords"    content="Alex Lynd, blog, electronics, programming, hacking, diy">
+  		<meta name="author"      content="Alex Lynd">
+		<meta name="viewport"    content="width=device-width, initial-scale=1">
+		
 		<link rel="stylesheet" type= "text/css" href="css/home.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="js/typed.js" type="text/javascript"></script>
 	</head>
 	<body>
@@ -23,10 +39,7 @@
 		<script src="js/particles.js"></script>
 		<script src="home.js"></script>
 		<div class= "text">
-			<header>
-				<?php include "menu.html";?>
-			</header>
-			
+			<header> <?php include "menu.html";?> </header>
 			<main>
 				<titlewrap><span class="typed" style="white-space:pre;"></span></titlewrap>
 				<p class= "float-fade-in">Welcome to my website! This is my personal page where you can find my project documentation, portfolio, story, and other resources/tutorials on a variety of things including programming, hacking and more. This site is programmed and maintained by me, and you can view the source <a href="https://github.com/alexlynd/website">here</a>.  Click the icons below to head to my social media, or one of the links in the menu to check out my stuff!</p>
