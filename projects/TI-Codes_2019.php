4c54c3ee4b94829ca1249a48ed047394332195bf
AlexLynd <alexlynd@protonmail.com>
2019-05-17 21:43:27 -0700

    Content

--- projects/TI-Codes_2019.php
+++ projects/TI-Codes_2019.php
@@ -19,10 +19,13 @@
 			<div class= "description">TI-Codes 2019 | A smart compost system with TI tech</div>
 		</header>
 		<main>
-
-			 <h3>Round 1: &nbsp; Description</h3>
+			<h3>Round 1: &nbsp; Description</h3>
 			<p>
 				The prolific impact of excess trash in our society has caused a multitude of issues stemming from a simple problem: poor organic waste management. Landfills are replete with compostables that can easily be repurposed to create valuable fertilizer. When organic waste decays inside a landfill it produces methane, a greenhouse gas, which contributes to air pollution and climate change. However, when organic waste is composted, fertile, nutrient-rich soil is created, providing a sustainable food source for deficient areas. While this may seem trivial, maintaining a compost system can be overwhelming and thus many people do not, even considering the benefits. As IOT devices become more prevalent to manage our daily lives, we propose the creation of an IOT composting system with integrated sensors and Wi-Fi connectivity to collect data and make smart predictions for simple maintenance. This system allows a user to easily monitor compost and gather data such as when to adjust water levels, add/remove compost, and other essential information. We plan on creating the following two-part system. The first node is comprised of a TI-Launchpad coupled with an ESP8266 and various sensors connected to the compost bin, gathering data to be transmitted over Wi-Fi. The other node is simply a TI calculator which we will rig with an ESP8266 to be utilized as a handheld receiving module, allowing us to monitor the compost remotely. This system is very modular and easy to implement, and we chose to design it this way for ease of access. The monitoring station will be able to gather data such as humidity, water level, and analyze this data to ensure compost is ready, and to make predictions in the future for optimal compost use. This system will allow people to use technology on hand (like calculators), with a few other parts to easily start helping the environment at home, while teaching the value of technology.
+			</p>
+			<p>
+				
+			
 			</p>
 			<hr>
 			<h3>Round 2: &nbsp; Design</h3>
