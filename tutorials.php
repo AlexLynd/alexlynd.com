ec587b5739cea4ad2772c124d3e866db2d0efa56
Alex Lynd <alexlynd@protonmail.com>
2019-05-19 21:10:55 -0700

    fixed style

--- tutorials.php
+++ tutorials.php
@@ -1,24 +1,22 @@
+<!-- PROJECTS -->
 <!doctype html>
 <html lang= "en-US">
 	<head>
 	    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<title>Tutorials</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type= "text/css" href="/css/portfolio.css">
-		<link rel="stylesheet" type= "text/css" href="/css/menu.css">
-		<script src="/js/typed.js" type="text/javascript"></script>
+		<link rel="stylesheet" type= "text/css" href="../css/portfolio.css">
+		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
-				<header>
-			<?php include "menu.html";?>
+		<?php include "menu.html";?>
+		<header>
+			<div class= "title">Tutorials</div>
+			<div class="description" id="sub-page">Tutorials.  Nothing here yet!</div>
+		</header>
+		<main>
 			
-		
-				
-			</header>
-			<main>
-				<div class= "title">Tutorials</div>
-				<center><p>Nothing here yet!</p></center>
-			</main>
+		</main>
 	</body>
 </html>	
