62ec2fbfaea9b18e609bb110793e6588598619e7
AlexLynd <alexlynd@protonmail.com>
2019-05-21 13:42:39 -0700

    relink tutorials page

---
+++ tutorials/index.php
@@ -0,0 +1,28 @@
+<!-- PROJECTS -->
+<!doctype html>
+<html lang= "en-US">
+	<head>
+	    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>Tutorials | Alex Lynd</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Tutorials and guides">
+  		<meta name="keywords"    content="Alex Lynd, blog, electronics, programming, hacking, diy, tutorials">
+  		<meta name="author"      content="Alex Lynd">
+		<meta name="viewport"    content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" type= "text/css" href="file:///C|/Users/alexander.lynd/Documents/GitHub/css/portfolio.css">
+		<link rel="stylesheet" type= "text/css" href="file:///C|/Users/alexander.lynd/Documents/GitHub/css/menu.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<?php include "../menu.html";?>
+		<header>
+			<div class= "title">Tutorials</div>
+			<div class="description" id="sub-page">Tutorials.  Nothing here yet!</div>
+			
+		</header>
+		<main>
+			
+		</main>
+	</body>
+</html>	
