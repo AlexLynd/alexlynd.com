1a61829ac404549fb53f9150ff195b3337f74732
AlexLynd <alexlynd@protonmail.com>
2019-05-22 19:31:30 -0700

    update portfolio links

--- donate.php
+++ donate.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Support me!</title>
-		<link rel="stylesheet" type= "text/css" href="css/portfolio.css">
+		<link rel="stylesheet" type= "text/css" href="css/post.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
