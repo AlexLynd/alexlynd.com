69eb54ed653912f6ff53d9e6811b732dc7d84692
AlexLynd <alexlynd@protonmail.com>
2019-05-23 11:13:49 -0700

    meta, remove css

--- donate.php
+++ donate.php
@@ -1,12 +1,16 @@
 <!doctype html>
 <html lang= "en-US">
 	<head>
+		<title>Support me! | Alex Lynd</title>
 		<meta charset="utf-8">
-		<title>Support me!</title>
+		<meta name="description" content="Tutorials and guides">
+  		<meta name="keywords"    content="Alex Lynd, blog, electronics, programming, hacking, diy, tutorials">
+  		<meta name="author"      content="Alex Lynd">
+		<meta name="viewport"    content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type= "text/css" href="css/post.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
+		
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
 	<body>
 		<?php include "menu.html";?>	
