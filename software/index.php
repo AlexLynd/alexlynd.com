b98d7a89195bf1f7af25ff9b646c730b87ba34eb
AlexLynd <alexlynd@protonmail.com>
2019-05-26 00:00:45 -0700

    y e e h a w
    no one cares about this anyways its just code for my website

---
+++ software/index.php
@@ -0,0 +1,24 @@
+<!-- PROJECTS -->
+<!doctype html>
+<html lang= "en-US">
+	<head>
+	    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>Projects</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type= "text/css" href="../css/post.css">
+		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<?php include "../menu.html";?>
+		<header>
+			<div class= "title">Software</div>
+			
+			<p class="sub-title"><a href="/projects">Projects</a>  <a href="">Software</a>  <a href="">Contests</a> <a href="">Portfolio</a></p>
+		</header>
+		<main id="wide">
+			<h3>PhoneGen</h3>
+			<p>A program to generate phone number wordlists for Wi-Fi hacking.</p>
+		</main>
+	</body>
+</html>	
