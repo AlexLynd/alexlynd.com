0bec3f9600a2b3303c3d2270808ed2fa36f80bf9
Alex Lynd <alexlynd@protonmail.com>
2019-05-31 22:57:48 -0700

    relink to style

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ feel free to snoop! | GitHub @alexlynd | Instagram @alex_lynd | Twitter @alexlyn
   		<meta name="author"      content="Alex Lynd">
 		<meta name="viewport"    content="width=device-width, initial-scale=1">
 		
-		<link rel="stylesheet" type= "text/css" href="css/post.css">
+		<link rel="stylesheet" type= "text/css" href="css/style.css">
 		<link rel="stylesheet" type= "text/css" href="css/menu.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 
 		<script src="js/typed.js" type="text/javascript"></script>
--- projects/CalcProbe/index.php
+++ projects/CalcProbe/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>AuditPi</title>
-		<link rel="stylesheet" type= "text/css" href="../../css/post.css">
+		<link rel="stylesheet" type= "text/css" href="../../css/style.css">
 		<link rel="stylesheet" type= "text/css" href="../../css/menu.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> 	
 	</head>
