2b3ee656d32ecc3d160e0c4bf3cae469f7150e12
Alex <alexlynd@protonmail.com>
2019-07-09 11:55:05 -0700

    Responsive design and content

--- index.php
+++ index.php
@@ -42,7 +42,9 @@ feel free to snoop! | GitHub @alexlynd | Instagram @alex_lynd | Twitter @alexlyn
 			<?php include "menu.html";?>
 			<main id="home">
 				<div class="centered">
+					<h1 class="tmp">Alex Lynd</h1>
 					<div class="title" style="font-size: 7vw"><span class="typed" style="white-space:pre;"></span></div>
+					<h3 class="fade-in">Hacker | Maker | Programmer</h3>
 					<p class= "float-fade-in">Welcome to my website! This is my personal page where you can find my project documentation, portfolio, story, and other resources/tutorials on a variety of things including programming, hacking and more. This site is programmed and maintained by me, and you can view the source <a href="https://github.com/alexlynd/website">here</a>.  Click the icons below to head to my social media, or one of the links in the menu to check out my stuff!</p>
 					<div class="iconwrap">
 						<a href="https://github.com/alexlynd" target="_blank"><i class="fab fa-github-alt"></i></a>
@@ -52,25 +54,33 @@ feel free to snoop! | GitHub @alexlynd | Instagram @alex_lynd | Twitter @alexlyn
 						<a href="https://patreon.com/alexlynd" target="_blank"><i class="fab fa-patreon"></i></a>
 					</div>
 				</div>
-		</main>
-		<div class="block" id="light">
-		<div class="description" id="home">
-				<h1>About Me</h1>
-				<p>I'm Alex Lynd, a 16 year old maker/hacker from California.  I pursue a lot of hobbies including piano, skating, TaeKwonDo, parkour, hacking and more!  I spend most of my time hacking electonics / programming, and push my content to the internet and YouTube.  I'm interested in infosec and hacking, and have been teaching myself a lot about various CS subjects.  I'm a big fan of open-source and free content, and my life goal is to keep learning and to spread my knowledge to as many people as I can.</p>
+				<div class= "arrow bounce">
+					<a href="#light">
+						<i class="fas fa-sort-down"></i>
+					</a>
+				</div>
+			</main>	
+			<div class="block" id="light" >
+				<div class="description" id="home">
+					<h1>About Me</h1>
+					<p>I'm Alex Lynd, a 16 year old maker/hacker from California.  I pursue a lot of hobbies including piano, skating, TaeKwonDo, parkour, hacking and more!  I spend most of my time hacking electonics / programming, and push my content to the internet and YouTube.  I'm interested in infosec and hacking, and have been teaching myself a lot about various CS subjects.  I'm a big fan of open-source and free content, and my goal is to keep learning and to spread my knowledge to as many people as I can.</p>
+				</div>
+				<div class="image" id="home">
+					<img src="img/profile.png" >
+				</div>
 			</div>
-			<div class="image" id="home">
-				<img src="img/profile.png" >
-			</div>
-		</div>
-		<div class="block" id="dark">
-		<div class="image" id="home">
-				<img src="img/fire.jpg">
-			</div>
-			<div class="description" id="home">
-				<h1>My Work</h1>
-				<p>I'm currently in high school, and am self taught in everything CS.  I've worked on a lot of personal projects aimed mostly at hardware hacking and auditing devices, which I've recently started to upload to the internet.  I've picked up a lot in programming, hacking, electronics, and webdev.  I coded this website myself from scratch, and you can check out some of my <a href="projects">projects</a>.  I prototype electronics, hack/fix consumer electronics, make websites, and much more.  My current projects include a device for portable wardriving and an AI-based trash sorting device.  I also love pyrotechnics and fire stuff.</p>
-			</div>
-		</div>
-		</div>
+			<div class="block" id="dark">
+				<div class="image" id="home">
+					<img src="img/fire.jpg">
+				</div>
+				<div class="description" id="home">
+					<h1>My Work</h1>
+					<p>I'm currently in high school, and am self taught in everything CS.  I've worked on a lot of personal projects aimed mostly at hardware hacking and auditing devices, which I've recently started to upload to the internet.  I've picked up a lot in programming, hacking, electronics, and webdev.  I coded this website myself from scratch, and you can check out some of my <a href="projects">projects</a>.  I prototype electronics, hack/fix consumer electronics, make websites, and much more.  My current projects include a device for portable wardriving and an AI-based trash sorting device.  I also love pyrotechnics and fire stuff.</p>
+				</div>
+			</div>	
+			<footer>
+				<p>© 2019 | Alex Lynd</p>
+			</footer>
+		</div> <!--text-->
 	</body>
 </html>
