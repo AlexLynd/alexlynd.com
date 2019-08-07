4ba5fcc48b06942cc8ef4f5d1faf5375ee3efc7f
Alex <alexlynd@protonmail.com>
2019-08-07 16:05:03 -0700

    deleted

---
+++ projects/temp.php
@@ -0,0 +1,57 @@
+<!-- PROJECTS -->
+<!doctype html>
+<html lang= "en-US">
+	<head>
+		<title>Projects | Alex Lynd</title>
+		<link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico"/>
+
+		<meta charset="utf-8">
+		<meta name="description" content="My projects">
+  		<meta name="keywords"    content="Alex Lynd, projects">
+  		<meta name="author"      content="Alex Lynd">
+		<meta name="viewport"    content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type= "text/css" href="../css/style.css">
+		<link rel="stylesheet" type= "text/css" href="../css/menu.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<?php include "../menu.html";?>
+		<header>
+			<div class= "title" style="font-size: 50px;">Portfolio</div>
+		</header>
+		
+
+		<main class="project">
+			<div class="post-container"> 
+				<div class="post">
+					<div class="post-icon">
+						<a href="ESP-DriveBy"><img src="ESP-DriveBy/img/ESP-DriveBy.jpg"></a>
+					</div>
+					<div class="post-caption">
+						<h1>Projects</h1>
+						<p>See projects I've made and am maintaining</p>
+					</div>
+				</div>
+				<div class="post">
+					<div class="post-icon">
+						<a href= "AuditPi"><img src="AuditPi/img/AuditPi.jpg"></a>
+					</div>
+					<div class="post-caption">
+						<h1>Portfolio</h1>
+						<p>Comissioned work I've done for others and big projects</p>
+					</div>
+				</div>
+				<div class="post">
+					<div class="post-icon">
+						<a href= "IR-Tester"><img src="IR-Tester/img/IR-Tester.jpg"></a>
+					</div>
+					<div class="post-caption">
+						<h1>Contests</h1>
+						<p>See contests I've entered and relevant projects</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>	
