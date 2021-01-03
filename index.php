9d7a1f164eaee805c9f9af03e563303f4663d5d7
Alex <alexlynd@protonmail.com>
2021-01-03 14:23:50 -0800

    update home page

--- index.php
+++ index.php
@@ -20,60 +20,74 @@
 		<script src="js/particles-main.js"></script>
 		<div class= "text">
         <?php include "menu.html";?>
-        <script>
-            UIkit.modal.alert("This website is currently under repair as of June 2nd 2020 and I'm working on updating some features/articles.  Pardon the mess!")
-        </script>
+        <!-- <style>
+            .uk-sticky-placeholder {
+                position: relative;
+            }
+        </style> -->
+        <!-- <script>
+            UIkit.modal.alert("This website is always under repair cause I suck at webdev!")
+        </script> -->
 
         <div class="" >
             <div class="uk-section main">
                 <div class="uk-container uk-container-xlarge">
-                    <div class="centered">
-                        <h1>Alex Lynd</h1>
-                        <p class="uk-margin-medium uk-animation-slide-bottom-medium">Well rounded hacker and maker looking for more opportunities to create cool CS projects and learn as much as I can.  I'm passionate about making YouTube videos and sharing my content with others.  Currently in high school and pursuing multiple hobbies including martial arts and music.</p>
+                    <div class="uk-position-center welcome-text">
+                        <div uk-parallax="y: -300; easing: -3"><h1 class="uk-animation-slide-bottom-medium" >Alex Lynd</h1></div>
+                        <div uk-parallax="y: -300; easing: -2.5"><p  class="uk-margin-medium uk-animation-slide-bottom-medium ">A 17 year old Hacker, Maker, YouTuber, Musician, and Ninja trying to change the world one hack at a time.</p></div>
+                        <p uk-margin uk-parallax="y: -300; easing: -2">
+                            <button class="uk-button uk-button-default uk-animation-slide-top">Contact Me</button>
+                            <button class="uk-button uk-button-default uk-animation-slide-top">TL;DR this Site</button>
+                        </p>
                     </div>
                 </div>
-                <a href="#about" class="uk-icon-link uk-position-absolute uk-position-bottom-center uk-animation-slide-top" uk-icon="icon: chevron-down; ratio: 2.5"></a>
+                <div class="uk-position-absolute uk-position-bottom-center ">
+                <a href="#about" class="uk-icon-link uk-animation-slide-top" uk-icon="icon: chevron-down; ratio: 3" style="padding-bottom: 1rem;"></a>
+                </div>
 
-            </div>
-            <div class="uk-section-primary">
-                <div class="uk-container uk-container-xlarge uk-padding-large">
-                    
-                    <div class="uk-grid-match uk-grid-divider uk-child-width-1-2@m" uk-grid>
-                        <div class="home-sections">
-                            <h1>About Me</h1>
-                            <p>I'm currently in high school, and am self taught in everything CS. I've worked on a lot of personal projects aimed mostly at hardware hacking and auditing devices, which I've recently started to upload to the internet. I've picked up a lot in programming, hacking, electronics, and webdev. I coded this website myself from scratch, and you can check out some of my projects. I prototype electronics, hack/fix consumer electronics, make websites, and much more. My current projects include a device for portable wardriving and an AI-based trash sorting device. I also love pyrotechnics and fire stuff.</p>
-                        </div>
-                        <div>
-                            <div class="uk-cover-container" uk-scrollspy="cls:uk-animation-slide-right">
-                                <img data-src="img/alex.jpg" alt="" uk-img uk-cover>
-                                
-                            </div>
-                        </div>
-                    </div>
-                   
-                
-                </div>
             </div>
             <div class="uk-section-secondary">
                 <div class="uk-container uk-container-xlarge uk-padding-large">
-                    
-                    <div class="uk-grid-match uk-grid-divider uk-child-width-1-2@m" uk-grid>
-                        <div>
-                            <div class="uk-cover-container" uk-scrollspy="cls:uk-animation-slide-left">
-                                <img data-src="img/fire.jpg" alt="" uk-img uk-cover>
+                    <div class="uk-text uk-grid-large uk-grid-match" uk-grid>
+
+                        <div class="uk-width-2-3@m">
+                            <div class="home-sections uk-position-relative uk-position-center-left">
+                                <h1 class=" ">About Me</h1>
+                                <p>Hi I'm Alex! I'm a maker & hacker / open-source proponent who promotes the decentralized knowledge base of our community through the various projects I enjoy documenting on my Youtube Channel, and on this website.  I've worked with InfoSec professionals to create cool tools and projects, and am impassioned by environmental-oriented projects.  This is a placeholder.</p> 
                                 
                             </div>
                         </div>
-                        <div class="home-sections">
-                            <h1>My Story</h1>
-                            <p class=>I'm Alex Lynd, and a 16 year old maker/hacker from California. I pursue a lot of hobbies including piano, skating, TaeKwonDo, parkour, hacking and more! I spend most of my time hacking electonics / programming, and push my content to the internet and YouTube. I'm interested in infosec and hacking, and have been teaching myself a lot about various CS subjects. I'm a big fan of open-source and free content, and my goal is to keep learning and to spread my knowledge to as many people as I can.  </p>
+
+                        <div class="uk-width-expand@m" >
+                            <div class="uk-cover-container">
+                                <img class= " " src="img/me.jpg" uk-cover>
+                            </div>
+                            
+                            
+                            
+                            <!-- <div class="uk-card uk-card-default uk-card-body">1-3</div> -->
                         </div>
-                        
-                    </div>
-                   
+                    </div>               
                 
                 </div>
             </div>
+            <!-- <div class="uk-section-primary">
+                <div class="uk-container uk-container-xlarge uk-padding-large">
+                    
+                    <div class="uk-text-center" uk-grid>
+                        <div class="uk-width-2-3@m">
+                            <div class="uk-card uk-card-default uk-card-body">1-3</div>
+                        </div>
+                        <div class="uk-width-expand@m">
+                            <div class="uk-card uk-card-default uk-card-body">Expand</div>
+                        </div>
+                    </div>
+                    
+                    
+                   
+                
+                </div>
+            </div> -->
             <?php include "footer.html";?>
         </div>
         </div>
